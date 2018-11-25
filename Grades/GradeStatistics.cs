d924aa17a4ee04ab4baab992e0a7217f9feefbbd
w2 <william.washington@live.com>
2018-11-25 13:42:36 -0500

    Add project files.

---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    class GradeStatistics
+    {
+        public float HighestGrade;
+        public float LowestGrade;
+        public float AverageGrade;
+
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+       
+        
+
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+        }
+    }
+}
